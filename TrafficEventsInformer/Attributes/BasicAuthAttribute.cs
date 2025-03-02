1b548b7bf7234f109d7f8c0787827015228d5004
Tomas Mikulaj <mikulajt@gmail.com>
2025-03-02 14:05:15 +0100

    Add SyncEvents endpoint with basic auth attribute

---
+++ TrafficEventsInformer/Attributes/BasicAuthAttribute.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System.Text;
+using TrafficEventsInformer.Models.Configuration;
+
+namespace TrafficEventsInformer.Attributes
+{
+    public class BasicAuthAttribute : Attribute, IAuthorizationFilter
+    {
+        public void OnAuthorization(AuthorizationFilterContext context)
+        {
+            var config = context.HttpContext.RequestServices.GetRequiredService<IConfiguration>();
+            CommonTI commonTICredentials = config.GetSection("CommonTI").Get<CommonTI>();
+
+            var authHeader = context.HttpContext.Request.Headers["Authorization"].ToString();
+            if (string.IsNullOrEmpty(authHeader) || !authHeader.StartsWith("Basic "))
+            {
+                context.Result = new UnauthorizedResult();
+                return;
+            }
+
+            // Decode Base64 authorization header
+            var encodedCredentials = authHeader.Substring("Basic ".Length).Trim();
+            var decodedCredentials = Encoding.UTF8.GetString(Convert.FromBase64String(encodedCredentials));
+
+            // Extract username and password
+            var parts = decodedCredentials.Split(':', 2);
+            if (parts.Length != 2 || parts[0] != commonTICredentials.BasicAuthUsername || parts[1] != commonTICredentials.BasicAuthPassword)
+            {
+                context.Result = new UnauthorizedResult();
+            }
+        }
+    }
+}
--- TrafficEventsInformer/Models/Configuration/CommonTI.cs
+++ TrafficEventsInformer/Models/Configuration/CommonTI.cs
@@ -4,5 +4,7 @@
     {
         public string Username { get; set; }
         public string Password { get; set; }
+        public string BasicAuthUsername { get; set; }
+        public string BasicAuthPassword { get; set; }
     }
 }
